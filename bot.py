83d1af911f15f5c33f11e5ef6d0f28ff1f846b06
Sankar9360 <118946067+Sankar9360@users.noreply.github.com>
2026-09-23 22:51:02 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 
 # Railway Environment Variables வழியாக பெறப்படும்
-API_ID = int(os.environ.get(23990433))
+API_ID = 23990433
 API_HASH = os.environ.get("e6c4b6ee1933711bc4da9d7d17e1eb20")
 BOT_TOKEN = os.environ.get("6300056570:AAEtxFMyGlK6LexF0QEM8qduYH6GTF762co")
 TARGET_CHANNEL = os.environ.get("-1002164111160")  # எ.கா: @mychannel அல்லது -100xxxxxxxxxx
